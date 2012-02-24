44d55689285e5ddbaecceeb063c48b55d9c485f6
jean-philippe serafin <serafinjp@gmail.com>
2012-02-24 18:36:17 +0100

    Adds a better documentation pattern

---
+++ docs/_bin/documentor.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+from yaml import load, dump
+
+
+def extract_doc(source, topnamespace):
+    """Extract docs from scss file.
+    """
+    f = open(source, 'r')
+    file_content = f.read()
+
+    r = re.compile(r"/\*(.[^\*]*)\*/\n@mixin ([a-z\-]{1,})", re.DOTALL)
+
+    docs = []
+
+    current_namespace = {'label': None}
+
+    for doc, mixin in r.findall(file_content):
+        doc_tree = load(doc)
+        namespace = doc_tree['namespace']
+        if current_namespace['label'] != namespace:
+            if current_namespace['label'] is not None:
+                docs.append(current_namespace)
+            current_namespace = {'label': namespace, 'mixins': []}
+        doc_tree['label'] = mixin
+        current_namespace['mixins'].append(doc_tree)
+
+    docs.append(current_namespace)
+
+    return "- %s" % dump({topnamespace: {'label': topnamespace, 'namespaces': docs}})
+
+
+def put_docs_in_file(scss_file, source, flag, namespace, destination=None):
+    f = open(source, 'r')
+    file_content = f.read()
+    file_content = file_content.replace(flag,
+            extract_doc(scss_file, namespace))
+    f = open(destination, 'w')
+    f.write(file_content)
+
+
+put_docs_in_file("mixin/layout.scss", "docs/_sources/mixins.html",
+    "#{layout}", "layout", "docs/mixins.html")
+put_docs_in_file("mixin/component.scss", "docs/mixins.html",
+    "#{component}", "components", "docs/mixins.html")
