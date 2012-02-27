db7159976a149176387444fe4d9e243c4286e57d
jean-philippe serafin <serafinjp@gmail.com>
2012-02-27 10:20:28 +0100

    Adds style.scss docs

--- docs/_bin/documentor.py
+++ docs/_bin/documentor.py
@@ -41,7 +41,9 @@ def put_docs_in_file(scss_file, source, flag, namespace, destination=None):
     f.write(file_content)
 
 
-put_docs_in_file("mixin/layout.scss", "docs/_sources/mixins.html",
+put_docs_in_file("mixin/style.scss", "docs/_sources/mixins.html",
+    "#{style}", "style", "docs/mixins.html")
+put_docs_in_file("mixin/layout.scss", "docs/mixins.html",
     "#{layout}", "layout", "docs/mixins.html")
 put_docs_in_file("mixin/component.scss", "docs/mixins.html",
     "#{component}", "components", "docs/mixins.html")
